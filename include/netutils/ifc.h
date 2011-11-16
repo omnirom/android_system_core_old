55daeffeeea369a6675934a10b31a690e4b1caf1
Niranjan Pendharkar <npendhar@codeaurora.org>
2011-11-16 12:50:36 -0800

    core: Add API to retrieve mtu size
    Adding an API to libnetutils to retrieve mtu size of an interface
    Made few utility functions from ifc_utils public
    
    Change-Id: Iddec2dcd9f241b146d7f362e2df0724cf740d1f2
    (cherry picked from commit 108cded7d96cc347f36b5b78c404c5ebccce13d7)

--- include/netutils/ifc.h
+++ include/netutils/ifc.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2008, The Android Open Source Project
+ * Copyright (C) 2011, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
@@ -64,10 +65,12 @@ extern int ifc_remove_route(const char *ifname, const char *dst,
                             int prefix_length, const char *gw);
 extern int ifc_get_info(const char *name, in_addr_t *addr, int *prefixLength,
                         unsigned *flags);
-
 extern int ifc_configure(const char *ifname, in_addr_t address,
                          uint32_t prefixLength, in_addr_t gateway,
                          in_addr_t dns1, in_addr_t dns2);
+extern int ifc_get_mtu(const char *name, int *mtuSz);
+extern in_addr_t prefixLengthToIpv4Netmask(int prefix_length);
+extern int ipv4NetmaskToPrefixLength(in_addr_t mask);
 
 extern in_addr_t prefixLengthToIpv4Netmask(int prefix_length);
 
