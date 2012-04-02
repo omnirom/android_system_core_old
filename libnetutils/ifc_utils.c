22bbf386e3fdc3bd658143d6a90278a7ff46acf6
Niranjan Pendharkar <npendhar@codeaurora.org>
2012-04-02 16:41:59 -0700

    Do not add default route during ifc_configure
    Avoid duplicate default routes.
    Remove adding of default routes during interface configuration as
    route add/remove operation is managed by ConnectivityService via netd.
    (cherry picked from commit 4a48635c72d91cf8decb7653948fe0c76de46098)
    
    Change-Id: I982b95a44bf91d3850ca7752e307c1b1b6a66ecf

--- libnetutils/ifc_utils.c
+++ libnetutils/ifc_utils.c
@@ -1,17 +1,18 @@
 /*
+ * Copyright (c) 2011-2013 The Linux Foundation. All rights reserved.
  * Copyright 2008, The Android Open Source Project
  * Copyright (C) 2011, Code Aurora Forum. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
@@ -802,11 +803,14 @@ ifc_configure(const char *ifname,
         ifc_close();
         return -1;
     }
+
+#ifndef QCOM_HARDWARE
     if (ifc_create_default_route(ifname, gateway)) {
         printerr("failed to set default route %s: %s\n", ipaddr_to_string(gateway), strerror(errno));
         ifc_close();
         return -1;
     }
+#endif
 
     ifc_close();
 
