6455ed5fdad492d60d2a9c8c40353fc1f8adbb83
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-07-04 15:26:57 +0100

    init: Add a mechanism to extend the property service in device configs
    Instead of every so often adding a new set of stuff here for a specific
    device, let the device includes overload the property arrays. The
    new device_perms.h header documents it
    
    Change-Id: I477e27abebeb4b85abe5fcaf19333999b1625417

--- init/property_service.c
+++ init/property_service.c
@@ -49,6 +49,8 @@
 #include "util.h"
 #include "log.h"
 
+#include <device_perms.h>
+
 #define PERSISTENT_PROPERTY_DIR  "/data/property"
 
 static int persistent_properties_loaded = 0;
@@ -57,6 +59,7 @@ static int property_area_inited = 0;
 static int property_set_fd = -1;
 
 /* White list of permissions for setting property services. */
+#ifndef PROPERTY_PERMS
 struct {
     const char *prefix;
     unsigned int uid;
@@ -90,25 +93,21 @@ struct {
     { "service.adb.tcp.port", AID_SHELL,    0 },
     { "persist.sys.",     AID_SYSTEM,   0 },
     { "persist.service.", AID_SYSTEM,   0 },
+    { "persist.service.", AID_RADIO,    0 },
     { "persist.security.", AID_SYSTEM,   0 },
     { "persist.service.bdroid.", AID_BLUETOOTH,   0 },
     { "selinux."         , AID_SYSTEM,   0 },
-    { "net.pdp0",         AID_RADIO,    AID_RADIO },
-    { "net.pdp1",         AID_RADIO,    AID_RADIO },
-    { "net.pdp2",         AID_RADIO,    AID_RADIO },
-    { "net.pdp3",         AID_RADIO,    AID_RADIO },
-    { "net.pdp4",         AID_RADIO,    AID_RADIO },
-    { "net.vsnet0",       AID_RADIO,    AID_RADIO },
-    { "net.vsnet1",       AID_RADIO,    AID_RADIO },
-    { "net.vsnet2",       AID_RADIO,    AID_RADIO },
-    { "net.vsnet3",       AID_RADIO,    AID_RADIO },
+    { "net.pdp",          AID_RADIO,    AID_RADIO },
     { NULL, 0, 0 }
 };
+/* Avoid extending this array. Check device_perms.h */
+#endif
 
 /*
  * White list of UID that are allowed to start/stop services.
  * Currently there are no user apps that require.
  */
+#ifndef CONTROL_PERMS
 struct {
     const char *service;
     unsigned int uid;
@@ -116,12 +115,10 @@ struct {
 } control_perms[] = {
     { "dumpstate",AID_SHELL, AID_LOG },
     { "ril-daemon",AID_RADIO, AID_RADIO },
-    { "rawip_vsnet1",AID_RADIO, AID_RADIO },
-    { "rawip_vsnet2",AID_RADIO, AID_RADIO },
-    { "rawip_vsnet3",AID_RADIO, AID_RADIO },
-    { "rawip_vsnet4",AID_RADIO, AID_RADIO },
      {NULL, 0, 0 }
 };
+/* Avoid extending this array. Check device_perms.h */
+#endif
 
 typedef struct {
     void *data;
