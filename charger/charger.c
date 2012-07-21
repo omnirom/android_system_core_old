12f45c7bf7a7c64dc3bccbfbf9cc58579e797839
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-07-21 22:09:37 +0700

    charger: allow home button to wake
    On certain devices (e.g. galaxysmtd), the user expects the physical
    home button to be able to wake the device as well as the power button.
    
    Change-Id: If764f36aba3810dfa5233af8baf0a740b04889e0

--- charger/charger.c
+++ charger/charger.c
@@ -894,6 +894,9 @@ static void process_key(struct charger *charger, int code, int64_t now)
                 kick_animation(charger->batt_anim);
             }
         }
+    } else {
+        if (key->pending)
+            kick_animation(charger->batt_anim);
     }
 
     key->pending = false;
@@ -902,6 +905,7 @@ static void process_key(struct charger *charger, int code, int64_t now)
 static void handle_input_state(struct charger *charger, int64_t now)
 {
     process_key(charger, KEY_POWER, now);
+    process_key(charger, KEY_HOME, now);
 
     if (charger->next_key_check != -1 && now > charger->next_key_check)
         charger->next_key_check = -1;
