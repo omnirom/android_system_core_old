810de2f369f71be47a2aaff328b78dde27dcacc5
David Ng <dave@codeaurora.org>
2012-08-23 20:40:33 -0700

    [1/11]init: Add support for properties expansion in exec command
    Add Android properties expansion to 'exec' arguments.
    E.g.  exec /system/bin/log ${sys.usb.config}
    
    CRs-Fixed: 390942
    Signed-off-by: Androidsftw <androidsftwsub@gmail.com>
    
    Conflicts:
    	init/builtins.c
    
    Change-Id: Ie520396d951e578bd9f591734a45d2f460b8f51d

--- init/builtins.c
+++ init/builtins.c
@@ -246,12 +246,32 @@ int do_exec(int nargs, char **args)
     pid_t pid;
     int status, i, j;
     char *par[MAX_PARAMETERS];
+    char prop_val[PROP_VALUE_MAX];
+    int len;
+
     if (nargs > MAX_PARAMETERS)
     {
         return -1;
     }
     for(i=0, j=1; i<(nargs-1) ;i++,j++)
     {
+        if ((args[j])
+            &&
+            (!expand_props(prop_val, args[j], sizeof(prop_val))))
+
+        {
+            len = strlen(args[j]);
+            if (strlen(prop_val) <= len) {
+                /* Overwrite arg with expansion.
+                 *
+                 * For now, only allow an expansion length that
+                 * can fit within the original arg length to
+                 * avoid extra allocations.
+                 * On failure, use original argument.
+                 */
+                strncpy(args[j], prop_val, len + 1);
+            }
+        }
         par[i] = args[j];
     }
     par[i] = (char*)0;
