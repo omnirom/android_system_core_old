fe7909fa7fb0544e4c19c89094a318d444f2d632
dhacker29 <davidhackerdvm@gmail.com>
2012-11-02 11:43:02 -0500

    [4/11]Allow a device to define the max number of service groups in init.h
    Certain proprieatary binaries (such as tcmd for instance) require
    more than 12 supplementary service groups.  By definiing:
    TARGET_NR_SVC_SUPP_GIDS := in BoardConfig this can now be changed
    per device.
    
    Depends on: https://gerrit.omnirom.org/#/c/5075/
    
    Conflicts:
    	init/Android.mk
    
    Change-Id: I8f968adc21768ca90232ded7b7e1ffa68dc7ea78

--- init/Android.mk
+++ init/Android.mk
@@ -39,6 +39,14 @@ SYSTEM_CORE_INIT_DEFINES := BOARD_CHARGING_MODE_BOOTING_LPM \
     BOARD_LPM_BOOT_ARGUMENT_NAME \
     BOARD_LPM_BOOT_ARGUMENT_VALUE
 
+ifeq ($(BOARD_USE_NO_DEVFS_SETUP),true)
+LOCAL_CFLAGS += -DNO_DEVFS_SETUP
+endif
+
+ifneq ($(TARGET_NR_SVC_SUPP_GIDS),)
+LOCAL_CFLAGS += -DNR_SVC_SUPP_GIDS=$(TARGET_NR_SVC_SUPP_GIDS)
+endif
+
 $(foreach system_core_init_define,$(SYSTEM_CORE_INIT_DEFINES), \
   $(if $($(system_core_init_define)), \
     $(eval LOCAL_CFLAGS += -D$(system_core_init_define)=\"$($(system_core_init_define))\") \
