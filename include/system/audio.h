f9a688b1a064c53de8957b452ff2a8403b123c0b
Mingming Yin <mingming@codeaurora.org>
2012-12-27 17:40:25 -0800

    qcom-fm: audio: Add support for FM feature
    -Define audio stream for FM
    -Define audio source for FM RX and FM RX A2DP
    
    Change-Id: If24e4c32916f6614e3367b4c9f2cd828562e0347
    
    Conflicts:
    	include/system/audio.h
    
    Change-Id: I6dc13fa73270650da3fd991a944023e694a01f22

--- include/system/audio.h
+++ include/system/audio.h
@@ -47,6 +47,9 @@ typedef enum {
     AUDIO_STREAM_ENFORCED_AUDIBLE = 7, /* Sounds that cannot be muted by user and must be routed to speaker */
     AUDIO_STREAM_DTMF             = 8,
     AUDIO_STREAM_TTS              = 9,
+#ifdef QCOM_FM_ENABLED
+    AUDIO_STREAM_FM               = 10,
+#endif
 
     AUDIO_STREAM_CNT,
     AUDIO_STREAM_MAX              = AUDIO_STREAM_CNT - 1,
