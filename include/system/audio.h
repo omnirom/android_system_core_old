a352929321446b6d307f7e71d26a24c21ff4063b
Vicky Sehrawat <vickys@codeaurora.org>
2013-01-29 14:35:09 -0800

    system/core: New stream type and flag for incall music delivery
    - Add new stream type and flag for incall music delivery
    
    Change-Id: I54d0d820d2dbb888d2e0cebdca63be2856eb00d8

--- include/system/audio.h
+++ include/system/audio.h
@@ -50,6 +50,9 @@ typedef enum {
 #ifdef QCOM_FM_ENABLED
     AUDIO_STREAM_FM               = 10,
 #endif
+#ifdef QCOM_HARDWARE
+    AUDIO_STREAM_INCALL_MUSIC     = 11,
+#endif
 
     AUDIO_STREAM_CNT,
     AUDIO_STREAM_MAX              = AUDIO_STREAM_CNT - 1,
@@ -517,9 +520,10 @@ typedef enum {
     //Qualcomm Flags
     AUDIO_OUTPUT_FLAG_LPA = 0x1000,      // use LPA
     AUDIO_OUTPUT_FLAG_TUNNEL = 0x2000,   // use Tunnel
-    AUDIO_OUTPUT_FLAG_VOIP_RX = 0x4000   // use this flag in combination with DIRECT to
+    AUDIO_OUTPUT_FLAG_VOIP_RX = 0x4000,  // use this flag in combination with DIRECT to
                                          // indicate HAL to activate EC & NS
                                          // path for VOIP calls
+    AUDIO_OUTPUT_FLAG_INCALL_MUSIC = 0x8000 //use this flag for incall music delivery
 #endif
 } audio_output_flags_t;
 
