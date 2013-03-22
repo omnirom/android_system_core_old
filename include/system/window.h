c85504bfe94772f3e20b8f3d1690dfe9b3b7363b
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-03-22 21:39:09 +0000

    Fix the QCOM_BSP ifdef
    We only want this when the full BSP is enabled.
    
    Change-Id: I7bb7b0d274d1221c5bb566d4ac24419e1e134519

--- include/system/window.h
+++ include/system/window.h
@@ -266,7 +266,7 @@ enum {
     NATIVE_WINDOW_SET_BUFFERS_USER_DIMENSIONS = 15, /* private */
     NATIVE_WINDOW_SET_POST_TRANSFORM_CROP   = 16,   /* private */
     NATIVE_WINDOW_SET_BUFFERS_SIZE          = 17,   /* private */
-#ifdef QCOM_HARDWARE
+#ifdef QCOM_BSP
     NATIVE_WINDOW_UPDATE_BUFFERS_GEOMETRY   = 18,   /* private */
 #endif
 };
