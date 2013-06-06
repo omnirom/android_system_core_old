b674eb2e1a9232f07300251f19040506ff010d7c
Subhash Chandra Bose Naripeddy <snariped@codeaurora.org>
2013-06-05 19:44:47 -0700

    system/core: Add tunnel format frame size
    Add tunnel mode formats frame sizes to audio.h. This ensures frame
    size definition in one place.
    
    Change-Id: Ia01cb4cba003e538a53aaa18330ef860367859c1

--- include/system/audio.h
+++ include/system/audio.h
@@ -32,6 +32,12 @@ __BEGIN_DECLS
  * frameworks/base/include/media/AudioSystem.h
  */
 
+#define AMR_FRAMESIZE 32
+#define QCELP_FRAMESIZE 35
+#define EVRC_FRAMESIZE 23
+#define AMR_WB_FRAMESIZE 61
+#define AAC_FRAMESIZE 2048
+
 typedef int audio_io_handle_t;
 
 /* Audio stream types */
