106c066369a0abfa5975a55eb59372ace5db06f5
Sridhar Gujje <sgujje@codeaurora.org>
2013-09-12 18:04:08 +0530

    Initial bringup system-core
    Qcril : Added diag permission to Ril
    
    diag: Remove world read/write permissions from /dev/diag
    
    /dev/diag has world read/write permissions which is considered
    a security risk.  Remove these permissions.  Add new qcom_diag
    group for access to /dev/diag
    
    CRs-Fixed: 356415
    
    Change-Id: I41c950d9b24742b25c4eada63d62d493550c384d

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -93,8 +93,10 @@
 #define AID_NET_BW_STATS  3006  /* read bandwidth statistics */
 #define AID_NET_BW_ACCT   3007  /* change bandwidth statistics accounting */
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
+#define AID_QCOM_DIAG     3009  /* can read/write /dev/diag */
 
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
+
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
 
@@ -169,6 +171,7 @@ static const struct android_id_info android_ids[] = {
     { "net_bw_stats",  AID_NET_BW_STATS, },
     { "net_bw_acct",   AID_NET_BW_ACCT, },
     { "net_bt_stack",  AID_NET_BT_STACK, },
+    { "qcom_diag",     AID_QCOM_DIAG, },
 
     { "everybody",     AID_EVERYBODY, },
     { "misc",          AID_MISC, },
