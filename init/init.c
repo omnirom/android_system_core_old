05ec86e4118f39a3b0c11f24965300aaf1a505c0
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-09-13 20:53:55 +0000

    init: fix indentation and tab
    Change-Id: Ia832ab4df5d955b2fa2b791baf9444aca21c2571
    
    Conflicts:
    
    	init/init.c

--- init/init.c
+++ init/init.c
@@ -815,19 +815,19 @@ int audit_callback(void *data, security_class_t cls, char *buf, size_t len)
 static int charging_mode_booting(void)
 {
 #ifndef BOARD_CHARGING_MODE_BOOTING_LPM
-	return 0;
+    return 0;
 #else
-	int f;
-	char cmb;
-	f = open(BOARD_CHARGING_MODE_BOOTING_LPM, O_RDONLY);
-	if (f < 0)
-		return 0;
+    int f;
+    char cmb;
+    f = open(BOARD_CHARGING_MODE_BOOTING_LPM, O_RDONLY);
+    if (f < 0)
+        return 0;
 
-	if (1 != read(f, (void *)&cmb,1))
-		return 0;
+    if (1 != read(f, (void *)&cmb,1))
+        return 0;
 
-	close(f);
-	return ('1' == cmb);
+    close(f);
+    return ('1' == cmb);
 #endif
 }
 
