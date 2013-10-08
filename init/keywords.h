dc548352fc5161fbdd2e0ec726e54d4033917519
Jens Andersen <jens.andersen@gmail.com>
2013-10-08 18:10:46 +0200

    Add support for init mknod command.
    This is required for statically allocated devices.
    These don't hit ueventd and hence doesn't automatically get a device node.
    
    Change-Id: I83d19f0666e1f1bbb369b226fba019994d403a89

--- init/keywords.h
+++ init/keywords.h
@@ -42,6 +42,8 @@ int do_loglevel(int nargs, char **args);
 int do_load_persist_props(int nargs, char **args);
 int do_load_all_props(int nargs, char **args);
 int do_wait(int nargs, char **args);
+int do_mknod(int nargs, char **args);
+
 #define __MAKE_KEYWORD_ENUM__
 #define KEYWORD(symbol, flags, nargs, func) K_##symbol,
 enum {
@@ -103,6 +105,7 @@ enum {
     KEYWORD(chown,       COMMAND, 2, do_chown)
     KEYWORD(chmod,       COMMAND, 2, do_chmod)
     KEYWORD(loglevel,    COMMAND, 1, do_loglevel)
+    KEYWORD(mknod,       COMMAND, 4, do_mknod)
     KEYWORD(load_persist_props,    COMMAND, 0, do_load_persist_props)
     KEYWORD(load_all_props,        COMMAND, 0, do_load_all_props)
     KEYWORD(ioprio,      OPTION,  0, 0)
