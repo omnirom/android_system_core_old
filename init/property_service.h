d61552e42e97319d7a8e9fc1e9e866430f630469
Vojtech Bocek <vbocek@gmail.com>
2013-12-07 19:43:12 +0100

    [2/2] init: support using old property system via makefile flag
    * In 4.4, Google switched to new property system which is based on
      binary tree instead of array of strings. It is faster, but old
      updater-binaries in ZIP files flashed in recovery are not compatible
      with new system, causing inability to flash older ZIPs in TWRP built
      in 4.4 tree.
    * This patch backports old system into standalone file and modifies
      the header to be compatible with both
    * To turn this on, specify following in device's BoardConfig.mk:
    
        BUILD_OLD_SYS_PROPS := true
        COMMON_GLOBAL_CFLAGS += -DBUILD_OLD_SYS_PROPS
    
    Signed-off-by: Vojtech Bocek <vbocek@gmail.com>
    
    Change-Id: Ie77897eb2f5a7a4d835cb0ad3405bd205a579c40

--- init/property_service.h
+++ init/property_service.h
@@ -20,6 +20,8 @@
 #include <stdbool.h>
 #include <sys/system_properties.h>
 
+#ifndef BUILD_OLD_SYS_PROPS
+
 extern void handle_property_set_fd(void);
 extern void property_init(void);
 extern void property_load_boot_defaults(void);
@@ -46,5 +48,20 @@ int property_get(const char *name, char *value)
 
     return __property_get(name, value);
 }
+
+#else /* BUILD_OLD_SYS_PROPS */
+
+extern void handle_property_set_fd(void);
+extern void property_init(void);
+extern void property_load_boot_defaults(void);
+extern void load_persist_props(void);
+extern void start_property_service(void);
+void get_property_workspace(int *fd, int *sz);
+extern int property_get(const char *name, char *value);
+extern int property_set(const char *name, const char *value);
+extern int properties_inited();
+int get_property_set_fd(void);
+
+#endif /* BUILD_OLD_SYS_PROPS */
 
 #endif	/* _INIT_PROPERTY_H */
