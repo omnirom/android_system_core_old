454f276d10b8f1f193d649ba5634a43438c02c15
xplodwild <xplod@ouverta.fr>
2014-01-05 17:34:01 +0100

    libutils: Add more MediaTek backcompat hacks
    Change-Id: I168ffdf5ad73ac80c121f47c6962f3644e86b1ef

---
+++ libutils/MediatekHacks.cpp
@@ -0,0 +1,6 @@
+extern "C" {
+    void _ZN7android6Tracer4initEv(){}
+    volatile int _ZN7android6Tracer8sIsReadyE = 1;
+    unsigned long long _ZN7android6Tracer12sEnabledTagsE = 0;
+    int _ZN7android6Tracer8sTraceFDE = -1;
+}
