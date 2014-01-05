634b81aa1614439add9eb70f9c46cca6d292a65b
xplodwild <xplod@ouverta.fr>
2014-01-05 17:35:41 +0100

    libutils: CallStack: Restore original update method
    Change-Id: Ie8501b2d6246204f2a4c1b1329aef57db0b9f930

--- include/utils/CallStack.h
+++ include/utils/CallStack.h
@@ -73,6 +73,8 @@ public:
              android_LogPriority priority = ANDROID_LOG_DEBUG,
              const char* prefix = 0) const;
 #ifdef MTK_MT6589
+    void update(int32_t ignoreDepth=1, int32_t maxDepth=MAX_DEPTH);
+
     // Dump a stack trace to the log
     void dump(const char* prefix = 0) const;
 #endif
