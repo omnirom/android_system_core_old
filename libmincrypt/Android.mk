cf680efda14450d28a50cb89f4881d51414758a3
wantowan2 <wantowan2@gmail.com>
2014-01-27 04:01:14 -0500

    [2/3] Restoring sdcard0 mount for Samsung msm8660 devices
    Change-Id: I6018093133d479400069a6a0f211620f89a391fc

--- libmincrypt/Android.mk
+++ libmincrypt/Android.mk
@@ -7,6 +7,13 @@ LOCAL_MODULE := libmincrypt
 LOCAL_SRC_FILES := rsa.c sha.c sha256.c
 include $(BUILD_STATIC_LIBRARY)
 
+## Crippled version without an RSA implementation
+## to coexist with libcrypto_static and provide SHA_hash
+include $(CLEAR_VARS)
+LOCAL_MODULE := libminshacrypt
+LOCAL_SRC_FILES := sha.c sha256.c
+include $(BUILD_STATIC_LIBRARY)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libmincrypt
