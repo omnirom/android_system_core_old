09e03a591456a7bbbba4be04d3844aeca5858382
Humberto Borba <humberos@gmail.com>
2014-01-29 00:51:09 -0200

    Revert "sdcard: Allow building as a static library"
    For some reason (maybe gcc) the sdcard daemon is broken for some devices
    
    I can't reproduce this error but I got some logs below:
    
    from logcat
    http://paste.omnirom.org/view/380f6994
    
    from kernel
    http://paste.omnirom.org/view/3ebfe137
    
    This reverts commit 1e8b51531e4d9ff356da0c09da4bf2860d523c8c.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I56b0b8c0b50fdc53101494557c773ce717273bd0

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -1,15 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := sdcard.c
-LOCAL_MODULE := libsdcard
-LOCAL_CFLAGS := -Wall -Wno-unused-parameter
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_STATIC_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := main.c
-LOCAL_MODULE := sdcard
+LOCAL_SRC_FILES:= sdcard.c
+LOCAL_MODULE:= sdcard
 LOCAL_CFLAGS := -Wall -Wno-unused-parameter
-LOCAL_STATIC_LIBRARIES := libsdcard libc libcutils liblog
+
+LOCAL_SHARED_LIBRARIES := libc libcutils
+
 include $(BUILD_EXECUTABLE)
