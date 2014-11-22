67b9a21e9ff3f306007690bfbf9866d878a935d7
David Ng <dave@codeaurora.org>
2014-11-21 18:01:08 -0800

    ueventd: Fix bootdevice by-name/by-num link creation
    Create bootdevice by-name/by-num links only if the block
    device is the bootdevice.  This fixes the issue of
    bootdevice path being created incorrectly if the first
    reported device with by-name/by-num is not the bootdevice.
    
    It also fixes devices in which the bootdevice property
    doesn't exist, or the symlink is created by a later step in
    the init sequence (such as init.rc itself)
    
    Change-Id: Ifb68080e3149917f9ee88a4a421007f29e8d83d2

--- init/devices.cpp
+++ init/devices.cpp
@@ -546,6 +546,7 @@ static char **get_block_device_symlinks(struct uevent *uevent)
     char link_path[256];
     int link_num = 0;
     char *p;
+    int is_bootdevice = -1;
     int mtd_fd = -1;
     int nr;
     char mtd_name_path[256];
@@ -597,6 +598,13 @@ static char **get_block_device_symlinks(struct uevent *uevent)
         free(p);
     }
 
+    if (pdev && boot_device[0] != '\0' && strstr(device, boot_device)) {
+        make_link_init(link_path, "/dev/block/bootdevice");
+        is_bootdevice = 1;
+    } else {
+        is_bootdevice = 0;
+    }
+
     if (uevent->partition_name) {
         p = strdup(uevent->partition_name);
         sanitize(p);
@@ -606,11 +614,13 @@ static char **get_block_device_symlinks(struct uevent *uevent)
             link_num++;
         else
             links[link_num] = NULL;
-        if (asprintf(&links[link_num], "/dev/block/bootdevice/by-name/%s", p) > 0)
-            link_num++;
-        else
-            links[link_num] = NULL;
 
+        if (is_bootdevice > 0) {
+            if (asprintf(&links[link_num], "/dev/block/bootdevice/by-name/%s", p) > 0)
+                link_num++;
+            else
+                links[link_num] = NULL;
+        }
         free(p);
     }
 
@@ -620,10 +630,12 @@ static char **get_block_device_symlinks(struct uevent *uevent)
         else
             links[link_num] = NULL;
 
-        if (asprintf(&links[link_num], "/dev/block/bootdevice/by-num/p%d", uevent->partition_num) > 0)
-            link_num++;
-        else
-            links[link_num] = NULL;
+        if (is_bootdevice > 0) {
+            if (asprintf(&links[link_num], "/dev/block/bootdevice/by-num/p%d", uevent->partition_num) > 0)
+                link_num++;
+            else
+                links[link_num] = NULL;
+        }
     }
 
     slash = strrchr(uevent->path, '/');
@@ -631,11 +643,6 @@ static char **get_block_device_symlinks(struct uevent *uevent)
         link_num++;
     else
         links[link_num] = NULL;
-
-    if (pdev && boot_device[0] != '\0' && strstr(device, boot_device)) {
-        /* Create bootdevice symlink for platform boot stroage device */
-        make_link_init(link_path, "/dev/block/bootdevice");
-    }
 
     return links;
 }
