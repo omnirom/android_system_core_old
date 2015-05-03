11e3f39f6b64db342dcba1946b6757a282deb5b1
quanganh2627 <quanganh2627@gmail.com>
2015-05-03 22:16:39 +0700

    [x86] Software-accelerated Pixel Flinger
    This patch improves the speed of pixelflinger needed
    executables (recovery). It is slightly changed from the
    original source from Motorola that came with source
    code from the Razr I. It fixes the issue with 'lag'
    within the ui of all x86 build recovery's.
    
    Original source:
    I-50_SMI-28/system_core.tar.gz/download" target="_blank">http://sourceforge.net/projects/razr-i.motorola/files/razr-i/9.8.2I-50_SMI-28/system_core.tar.gz/download
    
    Android-ia implementation off the same code:
    https://github.com/android-ia/platform_system_core/commit/8d7070878d582206abe1e10461ccbd8005a09008
    
    Further modified by HazouPH <jgrharbers@gmail.com> as seen here:
    http://review.cyanogenmod.org/#/c/70896/
    
    And by quanganh2627 <quanganh2627@gmail.com> as seen here:
    http://review.cyanogenmod.org/#/c/97125/
    
    Change-Id: Iaff08fcbd7d6be0e27811c6529d24984389f2ff3

--- libpixelflinger/tests/codegen/Android.mk
+++ libpixelflinger/tests/codegen/Android.mk
@@ -1,8 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq ($(TARGET_ARCH),x86)
 LOCAL_SRC_FILES:= \
-	codegen.cpp.arm
+    codegen.cpp
+else
+LOCAL_SRC_FILES:= \
+    codegen.cpp.arm
+endif
 
 LOCAL_SHARED_LIBRARIES := \
 	libcutils \
@@ -10,6 +15,11 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_C_INCLUDES := \
 	system/core/libpixelflinger
+
+ifeq ($(TARGET_ARCH),x86)
+LOCAL_C_INCLUDES += \
+    $(TARGET_OUT_HEADERS)/libenc
+endif
 
 LOCAL_MODULE:= test-opengl-codegen
 
