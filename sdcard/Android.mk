aada183a833909e68ef92a5232b10208ff0df58e
Nikhilesh Reddy <reddyn@codeaurora.org>
2015-05-18 15:49:39 -0700

    sdcard : Use the fuse shortcircuit option
    Use the fuse shortcircuit option for faster read and
    write performance in the fuse mount points.
    
    Change-Id: Iee11451348443108c82b14d6856e4c95a47159d1

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -1,6 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+endif
 
 LOCAL_SRC_FILES := sdcard.c
 LOCAL_MODULE := sdcard
