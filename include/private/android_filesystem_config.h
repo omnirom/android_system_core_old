97507ead83cc1b612d2af7992f4881b846b168ec
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2015-07-10 17:31:50 -0700

    system: core: Add Sensors group
    Defining group for sensors.
    
    CRs-Fixed: 431081
    
    Change-Id: I58151f2fd727bded1b16adb3caeac85e27e44036

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -122,6 +122,8 @@
 #define AID_OEM_RESERVED_2_START 5000
 #define AID_OEM_RESERVED_2_END   5999
 
+#define AID_SENSORS       3011 /* access to /dev/socket/sensor_ctl_socket & QCCI/QCSI */
+
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
@@ -222,6 +224,8 @@ static const struct android_id_info android_ids[] = {
     { "net_bt_stack",  AID_NET_BT_STACK, },
     { "readproc",      AID_READPROC, },
     { "wakelock",      AID_WAKELOCK, },
+
+    { "sensors",       AID_SENSORS, },
 
     { "everybody",     AID_EVERYBODY, },
     { "misc",          AID_MISC, },
