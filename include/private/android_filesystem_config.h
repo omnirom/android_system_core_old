0be60f4f1c7817646bcb56dde484ee510fbb5b89
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2015-07-10 17:31:50 -0700

    system: core: Add Sensors group
    Defining group for sensors.
    
    CRs-Fixed: 431081
    
    Change-Id: I58151f2fd727bded1b16adb3caeac85e27e44036

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -99,6 +99,8 @@
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
 #define AID_QCOM_DIAG     3009  /* can read/write /dev/diag */
 
+#define AID_SENSORS       3011 /* access to /dev/socket/sensor_ctl_socket & QCCI/QCSI */
+
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
 
 #define AID_MISC          9998  /* access to misc storage */
@@ -184,6 +186,8 @@ static const struct android_id_info android_ids[] = {
     { "net_bw_acct",   AID_NET_BW_ACCT, },
     { "net_bt_stack",  AID_NET_BT_STACK, },
     { "qcom_diag",     AID_QCOM_DIAG, },
+
+    { "sensors",       AID_SENSORS, },
 
     { "everybody",     AID_EVERYBODY, },
     { "misc",          AID_MISC, },
