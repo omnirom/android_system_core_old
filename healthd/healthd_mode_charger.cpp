d0213facefa120cffa2fb9465a44ac757ae900d2
Chunmei Cai <ccai@codeaurora.org>
2015-09-06 15:43:32 +0800

    healthd: charger: Add board overrides in mode_charger
    Add following overrides in mode_charger to improve the off-mode charging
    user experience:
     1. Draw battery information on the animation, such as: capacity;
     2. Handle board specific battery status update;
     3. Handle board specific mode charger initialization;
     4. Turn on/off the backlight explicitely;
    
    Change-Id: Ic2b7ab6deeb52c4effe3b4af9b590950d5ee97f1
    
    healthd: Stop logspam
    
    Change-Id: Ib4a4cb226f61f3e2a4a7c59ca0e0bae47b468d6a

--- healthd/healthd_mode_charger.cpp
+++ healthd/healthd_mode_charger.cpp
@@ -408,6 +408,8 @@ static void draw_battery(const struct charger* charger)
     }
     draw_clock(anim);
     draw_percent(anim);
+
+    healthd_board_mode_charger_draw_battery(batt_prop);
 }
 
 static void redraw_screen(struct charger *charger)
@@ -479,6 +481,7 @@ static void update_screen_state(struct charger *charger, int64_t now)
         init_status_display(batt_anim);
 
 #ifndef CHARGER_DISABLE_INIT_BLANK
+        healthd_board_mode_charger_set_backlight(false);
         gr_fb_blank(true);
 #endif
         minui_inited = true;
@@ -488,6 +491,7 @@ static void update_screen_state(struct charger *charger, int64_t now)
     if (batt_anim->num_cycles > 0 && batt_anim->cur_cycle == batt_anim->num_cycles) {
         reset_animation(batt_anim);
         charger->next_screen_transition = -1;
+        healthd_board_mode_charger_set_backlight(false);
         gr_fb_blank(true);
         LOGV("[%" PRId64 "] animation done\n", now);
         if (charger->charger_connected)
@@ -521,9 +525,11 @@ static void update_screen_state(struct charger *charger, int64_t now)
         }
     }
 
-    /* unblank the screen  on first cycle */
-    if (batt_anim->cur_cycle == 0)
+    /* unblank the screen on first cycle */
+    if (batt_anim->cur_cycle == 0) {
         gr_fb_blank(false);
+        healthd_board_mode_charger_set_backlight(true);
+    }
 
     /* draw the new frame (@ cur_frame) */
     redraw_screen(charger);
@@ -621,6 +627,7 @@ static void set_next_key_check(struct charger *charger,
 
 static void process_key(struct charger *charger, int code, int64_t now)
 {
+    struct animation *batt_anim = charger->batt_anim;
     struct key_state *key = &charger->keys[code];
 
     if (code == KEY_POWER) {
@@ -647,17 +654,25 @@ static void process_key(struct charger *charger, int code, int64_t now)
                  * make sure we wake up at the right-ish time to check
                  */
                 set_next_key_check(charger, key, POWER_ON_KEY_TIME);
-
-               /* Turn on the display and kick animation on power-key press
-                * rather than on key release
-                */
-                kick_animation(charger->batt_anim);
-                request_suspend(false);
             }
         } else {
-            /* if the power key got released, force screen state cycle */
             if (key->pending) {
-                kick_animation(charger->batt_anim);
+                /* If key is pressed when the animation is not running, kick
+                 * the animation and quite suspend; If key is pressed when
+                 * the animation is running, turn off the animation and request
+                 * suspend.
+                 */
+                if (!batt_anim->run) {
+                    kick_animation(batt_anim);
+                    request_suspend(false);
+                } else {
+                    reset_animation(batt_anim);
+                    charger->next_screen_transition = -1;
+                    healthd_board_mode_charger_set_backlight(false);
+                    gr_fb_blank(true);
+                    if (charger->charger_connected)
+                        request_suspend(true);
+                }
             }
         }
     }
@@ -677,6 +692,8 @@ static void handle_power_supply_state(struct charger *charger, int64_t now)
 {
     if (!charger->have_battery_state)
         return;
+
+    healthd_board_mode_charger_battery_update(batt_prop);
 
     if (!charger->charger_connected) {
 
@@ -840,6 +857,8 @@ void healthd_mode_charger_init(struct healthd_config* config)
     dump_last_kmsg();
 
     LOGW("--------------- STARTING CHARGER MODE ---------------\n");
+
+    healthd_board_mode_charger_init();
 
     ret = ev_init(std::bind(&input_callback, charger, std::placeholders::_1,
                             std::placeholders::_2));
