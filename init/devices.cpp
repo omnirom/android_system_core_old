2b143b247a69deba1577dd1b2ca92138be2b48dc
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-26 03:23:16 +0100

    Revert "Revert "ueventd: Add bootdevice links support for selinux""
    This reverts commit 4a7c8e781738639c95f114042c0457e86fbb9018.
    
    PS2: add ifdefs for _PLATFORM_BASE
    PS5: Update ifdef to ifndef for devices where PLATFORM_BASE isn't
    specified
    
    needs:
    https://gerrit.omnirom.org/15809
    https://gerrit.omnirom.org/15810
    
    Change-Id: I0347e5a380b764a1a1afb1a94530eef76702b7e8

--- init/devices.cpp
+++ init/devices.cpp
@@ -536,10 +536,10 @@ static char **get_block_device_symlinks(struct uevent *uevent)
         return NULL;
     }
 
-    char **links = (char**) malloc(sizeof(char *) * 4);
+    char **links = (char**) malloc(sizeof(char *) * 6);
     if (!links)
         return NULL;
-    memset(links, 0, sizeof(char *) * 4);
+    memset(links, 0, sizeof(char *) * 6);
 
     INFO("found %s device %s\n", type, device);
 
@@ -554,6 +554,12 @@ static char **get_block_device_symlinks(struct uevent *uevent)
             link_num++;
         else
             links[link_num] = NULL;
+#ifdef _PLATFORM_BASE
+        if (asprintf(&links[link_num], "/dev/block/bootdevice/by-name/%s", p) > 0)
+            link_num++;
+        else
+            links[link_num] = NULL;
+#endif
         free(p);
     }
 
@@ -562,6 +568,12 @@ static char **get_block_device_symlinks(struct uevent *uevent)
             link_num++;
         else
             links[link_num] = NULL;
+#ifdef _PLATFORM_BASE
+        if (asprintf(&links[link_num], "/dev/block/bootdevice/by-num/p%d", uevent->partition_num) > 0)
+            link_num++;
+        else
+            links[link_num] = NULL;
+#endif
     }
 
     slash = strrchr(uevent->path, '/');
@@ -569,12 +581,12 @@ static char **get_block_device_symlinks(struct uevent *uevent)
         link_num++;
     else
         links[link_num] = NULL;
-
+#ifndef _PLATFORM_BASE
     if (pdev && boot_device[0] != '\0' && strstr(device, boot_device)) {
         /* Create bootdevice symlink for platform boot stroage device */
         make_link_init(link_path, "/dev/block/bootdevice");
     }
-
+#endif
     return links;
 }
 
