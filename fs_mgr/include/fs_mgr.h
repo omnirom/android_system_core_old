c8c07c6ae8b18bc506b06a7dcbe1c6c89200f455
maxwen <max.weninger@gmail.com>
2016-07-31 22:43:01 +0200

    [2/2] core: add support for TARGET_HW_DISK_ENCRYPTION
    Change-Id: Id6c90b45cd528c640e25a7a739042a27cf588c8d

--- fs_mgr/include/fs_mgr.h
+++ fs_mgr/include/fs_mgr.h
@@ -108,6 +108,7 @@ int fs_mgr_is_notrim(struct fstab_rec *fstab);
 int fs_mgr_is_formattable(struct fstab_rec *fstab);
 int fs_mgr_is_nofail(struct fstab_rec *fstab);
 int fs_mgr_swapon_all(struct fstab *fstab);
+int fs_mgr_is_mdtp_activated(void);
 
 int fs_mgr_do_format(struct fstab_rec *fstab);
 
