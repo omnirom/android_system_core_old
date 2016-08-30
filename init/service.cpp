60484f59e7f32e202d2873e45a05d41235a653c4
William Bellavance <flintman@flintmancomputers.com>
2016-08-30 08:04:38 -0400

    init: don't skip starting a service with no domain if permissive
    Requires external/libselinux change as well
    
    [AdrianDC] Add an information about allowed permissive services
    
    Change-Id: I3f2887930e15d09014c2594141ba4acbbc8d6d9d

--- init/service.cpp
+++ init/service.cpp
@@ -80,7 +80,10 @@ static std::string ComputeContextFromExecutable(std::string& service_name,
     }
     if (rc == 0 && computed_context == mycon.get()) {
         LOG(ERROR) << "service " << service_name << " does not have a SELinux domain defined";
-        return "";
+        if (selinux_status_getenforce() > 0) {
+            return "";
+        }
+        LOG(INFO) << "Service " << service_name << " allowed on permissive init";
     }
     if (rc < 0) {
         LOG(ERROR) << "could not get context while starting '" << service_name << "'";
