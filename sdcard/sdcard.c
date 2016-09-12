80dc1276e22a891bca6939f99263cb336c162388
Keith Mok <kmok@cyngn.com>
2016-09-12 15:59:09 -0400

    sdcard: add FUSE_STACKED_IO
    Some kernel use FUSE_STACKED_IO
    instead of FUSE_SHORTCIRCUIT
    
    Change-Id: I6da68acba9d3a86f896743d161d63b2acc3ab571

--- sdcard/sdcard.c
+++ sdcard/sdcard.c
@@ -1221,8 +1221,7 @@ static int handle_open(struct fuse* fuse, struct fuse_handler* handler,
     }
     out.fh = ptr_to_id(h);
     out.open_flags = 0;
-
-    #ifdef FUSE_SHORTCIRCUIT
+    #if defined(FUSE_SHORTCIRCUIT) || defined(FUSE_STACKED_IO)
         out.lower_fd = h->fd;
     #else
         out.padding = 0;
@@ -1391,8 +1390,7 @@ static int handle_opendir(struct fuse* fuse, struct fuse_handler* handler,
     }
     out.fh = ptr_to_id(h);
     out.open_flags = 0;
-
-    #ifdef FUSE_SHORTCIRCUIT
+    #if defined(FUSE_SHORTCIRCUIT) || defined(FUSE_STACKED_IO)
         out.lower_fd = -1;
     #else
         out.padding = 0;
@@ -1480,6 +1478,9 @@ static int handle_init(struct fuse* fuse, struct fuse_handler* handler,
     out.max_readahead = req->max_readahead;
     out.flags = FUSE_ATOMIC_O_TRUNC | FUSE_BIG_WRITES;
 
+    #ifdef FUSE_STACKED_IO
+        out.flags |= FUSE_STACKED_IO;
+    #endif
     #ifdef FUSE_SHORTCIRCUIT
         out.flags |= FUSE_SHORTCIRCUIT;
     #endif
