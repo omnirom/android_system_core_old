1b6fdec844842e5ec8faf5d2c407a335f4e137e0
maxwen <max.weninger@gmail.com>
2016-09-20 23:33:36 +0200

    healthd: dont be so chatty in kernel log
    Change-Id: Icc37ef6811aaed2d58ec29f79be049c3cc5dcb52

--- healthd/healthd_board_default.cpp
+++ healthd/healthd_board_default.cpp
@@ -25,5 +25,5 @@ void healthd_board_init(struct healthd_config*)
 int healthd_board_battery_update(struct android::BatteryProperties*)
 {
     // return 0 to log periodic polled battery status to kernel log
-    return 0;
+    return 1;
 }
