0378d1ae6a6ae60e91348bce620a8cb3ef4a9290
Keun-young Park <keunyoung@google.com>
2017-07-05 11:38:44 -0700

    add "shutdown critical" to service
    - "shutdown critical" prevents killing the service during
      shutdown. And the service will be started if not running.
    - Without it, services will be killed by SIGTERM / SIGKILL during shutdown.
    - Even services with "shutdown critical" will be killed if shutdown
      times out.
    - Removes ueventd and vold from hard coded list. Each service's rc will
      be updated to add "shutdown critical". watchdogd is still kept in the list.
    
    bug: 37626581
    Test: reboot and check last kmsg
    
    Change-Id: Ie8cc699d1efbc59b9a2561bdd40fec64aed5a4bb
    (cherry picked from commit cccb34fce8798f2a359c6eb0e394ef9460673629)

--- init/service.cpp
+++ init/service.cpp
@@ -479,6 +479,14 @@ bool Service::ParseSetenv(const std::vector<std::string>& args, std::string* err
     return true;
 }
 
+bool Service::ParseShutdown(const std::vector<std::string>& args, std::string* err) {
+    if (args[1] == "critical") {
+        flags_ |= SVC_SHUTDOWN_CRITICAL;
+        return true;
+    }
+    return false;
+}
+
 template <typename T>
 bool Service::AddDescriptor(const std::vector<std::string>& args, std::string* err) {
     int perm = args.size() > 3 ? std::strtoul(args[3].c_str(), 0, 8) : -1;
@@ -562,6 +570,7 @@ Service::OptionParserMap::Map& Service::OptionParserMap::map() const {
         {"namespace",   {1,     2,    &Service::ParseNamespace}},
         {"seclabel",    {1,     1,    &Service::ParseSeclabel}},
         {"setenv",      {2,     2,    &Service::ParseSetenv}},
+        {"shutdown",    {1,     1,    &Service::ParseShutdown}},
         {"socket",      {3,     6,    &Service::ParseSocket}},
         {"file",        {2,     2,    &Service::ParseFile}},
         {"user",        {1,     1,    &Service::ParseUser}},
