f95edec44f0fcd092cedaaab30f8187595c25c38
maxwen <max.weninger@gmail.com>
2017-10-02 00:03:49 +0200

    healthd: cover devices that have voltage_max file with value of 0
    its perfectly fine to use default value also in those cases
    instead of not adding anything at all and breaking
    fast vs. slow charging calc in framework
    
    Change-Id: I098660592dcb1c877ad15ed2b5c3b40cc2d414e6

--- healthd/BatteryMonitor.cpp
+++ healthd/BatteryMonitor.cpp
@@ -278,7 +278,10 @@ bool BatteryMonitor::update(void) {
             int ChargingVoltage =
                 (access(path.string(), R_OK) == 0) ? getIntField(path) :
                 DEFAULT_VBUS_VOLTAGE;
-
+            // there are devices that have the file but with a value of 0
+            if (ChargingVoltage == 0) {
+                ChargingVoltage = DEFAULT_VBUS_VOLTAGE;
+            }
             double power = ((double)ChargingCurrent / MILLION) *
                            ((double)ChargingVoltage / MILLION);
             if (MaxPower < power) {
