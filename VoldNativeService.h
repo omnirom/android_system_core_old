3bfb996597fed33891dffe939156387cda8afe54
Daniel Rosenberg <drosen@google.com>
2018-10-12 16:24:21 -0700

    Remove spurious function declaration
    Test: Nothing changes
    Change-Id: I27f0d832d33099efe25794987c307d773b5d987c
    Merged-In: Icba16578608a6cbf922472e9d4ae5b8cf5f016c6

--- VoldNativeService.h
+++ VoldNativeService.h
@@ -111,9 +111,6 @@ class VoldNativeService : public BinderService<VoldNativeService>, public os::Bn
     binder::Status destroyUserStorage(const std::unique_ptr<std::string>& uuid, int32_t userId,
                                       int32_t flags);
 
-    binder::Status mountExternalStorageForApp(const std::string& packageName, int32_t appId,
-                                              const std::string& sandboxId, int32_t userId);
-
     binder::Status startCheckpoint(int32_t retry);
     binder::Status needsCheckpoint(bool* _aidl_return);
     binder::Status needsRollback(bool* _aidl_return);
