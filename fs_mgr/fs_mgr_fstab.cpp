ba997697f80cee3074179d6290934ebdb76081c4
dianlujitao <dianlujitao@lineageos.org>
2019-09-15 19:46:14 +0800

    Squashed wrapped key support?
    Change-Id: Ibdf035e548c3f5085401f60df158c9a327947f33

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -211,6 +211,7 @@ void ParseFsMgrFlags(const std::string& flags, FstabEntry* entry) {
         CheckFlag("first_stage_mount", first_stage_mount);
         CheckFlag("slotselect_other", slot_select_other);
         CheckFlag("fsverity", fs_verity);
+        CheckFlag("wrappedkey", wrapped_key);
 
 #undef CheckFlag
 
